1a2f4774e205267c988dff84ee2476f0ba341af8
MahnoorWaheed <mahnoorwaheed8395@gmail.com>
2025-01-25 23:09:50 +0500

    checked responsiveness through device preview

--- lib/widgets/app_bar.dart
+++ lib/widgets/app_bar.dart
@@ -3,14 +3,14 @@ import 'package:flutter/material.dart';
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   final String title;
   final List<Widget>? actions;
-  final Color backgroundColor;
+  
   final bool centerTitle;
 
   const CustomAppBar({
     Key? key,
     required this.title,
     this.actions,
-    this.backgroundColor = Colors.transparent,
+    
     this.centerTitle = true,
   }) : super(key: key);
 
@@ -21,8 +21,6 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
         title,
         style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
       ),
-     
-      backgroundColor: backgroundColor,
       elevation: 0,
       actions: actions,
     );
